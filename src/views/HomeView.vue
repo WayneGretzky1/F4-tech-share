b4204570f04ed4ba3dccb44df16f2a92cf2f104b
Calvin Chau <calvin.chau0@gmail.com>
2025-11-12 15:35:18 -0600

    added animations to input box, radio buttons, dropdown

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,12 @@
 <template>
   <div>
-    <!-- Header -->
-    <div class="header">
-      <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-        <ul class="pure-menu-list">
-          <li class="pure-menu-item pure-menu-selected">
-            <RouterLink to="/" class="pure-menu-link">Home</RouterLink>
-          </li>
-        </ul>
-      </div>
-    </div>
 
     <!-- Splash -->
     <div class="splash-container">
       <div class="splash">
         <h1 class="splash-head">Should the Minnesota Twins keep Pablo Lopez?</h1>
         <p class="splash-subhead">Consent to this survey and help out fellow Twins fans</p>
-        <p>
+        <p id="button-container">
           <RouterLink to="/survey" class="pure-button pure-button-primary">Consent</RouterLink>
           <RouterLink to="/decline" class="pure-button pure-button-primary">Decline</RouterLink>
         </p>
@@ -26,3 +16,25 @@
 </template>
 
 <!-- <style scoped src="../assets/home.css"></style> -->
+
+
+
+<style scoped>
+  .splash-container {
+    height: 100vh;
+    overflow: auto;
+  }
+
+  #button-container {
+    margin-bottom: 30px;
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .splash {
+    margin-top: 0;
+  }
+
+</style>
